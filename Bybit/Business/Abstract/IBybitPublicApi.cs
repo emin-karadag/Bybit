dcde20b5e91aadd27512dd9e2c3ee2157b66d29c
Mehmet Emin KARADAĞ <emin.karadag@ogr.sakarya.edu.tr>
2023-03-26 21:22:38 +0300

    Bybit Public API -> GetServerTimeAsync() metodu eklendi.

---
+++ Bybit/Business/Abstract/IBybitPublicApi.cs
@@ -0,0 +1,15 @@
+﻿using Bybit.Core.Results.Abstract;
+using Bybit.Entity.Models.Public;
+
+namespace Bybit.Business.Abstract
+{
+    public interface IBybitPublicApi
+    {
+        /// <summary>
+        /// Get the Bybit server timestamp
+        /// </summary>
+        /// <param name="ct">CancellationToken</param>
+        /// <returns></returns>
+        Task<IDataResult<ServerTimeData>> GetServerTimeAsync(CancellationToken ct = default);
+    }
+}
---
+++ Bybit/Business/Concrete/BybitPublicApi.cs
@@ -0,0 +1,33 @@
+﻿using Bybit.Business.Abstract;
+using Bybit.Core.Results.Abstract;
+using Bybit.Core.Results.Concrete;
+using Bybit.Core.Utilities;
+using Bybit.Entity.Models.Public;
+using System.Text.Json;
+
+namespace Bybit.Business.Concrete
+{
+    public class BybitPublicApi : IBybitPublicApi
+    {
+        private const string _prefix = "/public";
+
+        public async Task<IDataResult<ServerTimeData>> GetServerTimeAsync(CancellationToken ct = default)
+        {
+            try
+            {
+                var result = await RequestHelper.SendRequestAsync($"{_prefix}/time", version: "v3", ct: ct);
+                var data = JsonSerializer.Deserialize<ServerTimeModel>(result);
+
+                if (data is not null)
+                    return data.RetMsg == "OK"
+                        ? new SuccessDataResult<ServerTimeData>(data.Result, data.RetMsg, data.RetCode)
+                        : new ErrorDataResult<ServerTimeData>(data.RetMsg, data.RetCode);
+                return new SuccessDataResult<ServerTimeData>();
+            }
+            catch (Exception ex)
+            {
+                return new ErrorDataResult<ServerTimeData>(ex.Message);
+            }
+        }
+    }
+}
